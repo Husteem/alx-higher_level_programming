3c37749e7af23bd9a4e6fb465e5bdf2406aca1bc
Naim Hussain <117782392+Husteem@users.noreply.github.com>
2023-06-07 19:49:59 +0100

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a list of available attributes and methods of an object husteem"""
+
+
+def lookup(obj):
+    """Returns the list of an object's attributes"""
+    return(dir(obj))
