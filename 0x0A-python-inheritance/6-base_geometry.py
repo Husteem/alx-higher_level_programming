2383692dc14323d26b474dae314584c42331688e
Naim Hussain <117782392+Husteem@users.noreply.github.com>
2023-06-07 19:56:12 +0100

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry classc teem"""
+
+
+class BaseGeometry:
+    """This represents base geometry"""
+
+    def area(self):
+        """Umimplemented"""
+        raise Exception("area() is not implemented")
