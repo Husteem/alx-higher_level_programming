f2c02a5dd81f455130bd03a92ac0e19d49e781f0
Naim Hussain <117782392+Husteem@users.noreply.github.com>
2023-06-07 19:58:26 +0100

    Create 7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class husteem"""
+
+
+class BaseGeometry:
+    """This reprsents the base geometry"""
+
+    def area(self):
+        """Not implement yet"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Parameter validation as an integer"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
