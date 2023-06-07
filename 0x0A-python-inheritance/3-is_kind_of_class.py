4c4b43671079f500960aff76c80857caf58916c2
Naim Hussain <117782392+Husteem@users.noreply.github.com>
2023-06-07 19:53:34 +0100

    Create 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines an inherited class-checking function usteem"""
+
+
+def is_kind_of_class(obj, a_class):
+    """This checks if an object is an instance of,
+    or inherited instance of a class that inherited from,
+    the specified class.
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
