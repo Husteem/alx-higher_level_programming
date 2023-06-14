d62fe938d36968b302bac9dd9f18d24447f6f3ce
Naim Hussain <117782392+Husteem@users.noreply.github.com>
2023-06-14 06:04:32 +0100

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,12 +2,10 @@
 const myObject = {
   type: 'object',
   value: 12
-  
 };
 console.log(myObject);
 myObject.incr = function () {
   this.value++;
-  
 };
 myObject.incr();
 console.log(myObject);
