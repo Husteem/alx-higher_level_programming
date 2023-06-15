5f10e3d9eba62a4bf6a46f8444f30a768cbab3d3
Naim Hussain <117782392+Husteem@users.noreply.github.com>
2023-06-15 05:41:02 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,21 +1,11 @@
-#!/usr/bin/node
-
+!/usr/bin/node
 const { dict } = require('./101-data');
-
 const newDict = {};
-
 for (const userId in dict) {
-
   const occurrence = dict[userId];
-
   if (!newDict[occurrence]) {
-
     newDict[occurrence] = [];
-
   }
-
   newDict[occurrence].push(userId);
-
 }
-
 console.log(newDict);
