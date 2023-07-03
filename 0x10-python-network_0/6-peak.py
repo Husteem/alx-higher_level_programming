c432def3b3f89e365d306a617ed27a3a1622b18c
Naim Hussain <117782392+Husteem@users.noreply.github.com>
2023-07-03 06:00:42 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a function that finds a peak in a list of unsorted integers. teem""" 
+"""a function that finds a peak in a list of unsorted integers. teem"""
 
 
 def find_peak(list_of_integers):
