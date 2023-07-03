20825a7ebf5a2949c03ca6fa16f8431f25be5923
Naim Hussain <117782392+Husteem@users.noreply.github.com>
2023-07-03 05:53:59 +0100

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """a Python script that takes in a URL, sends a request to same and displays
-the value of the X-Request-Id variable found in the header of the response. teem
+the value of the X-Request-Id variable found in the header of the response.
 """
 import sys
 import urllib.request
